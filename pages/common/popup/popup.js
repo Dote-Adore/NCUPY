9555f108a520ecb591c516a8cd93167cdc0b3850
Dote-Adore <1379382440@qq.com>
2019-07-30 16:41:34 +0800

    1.0.9
    新增自定义motal的动画效果

--- pages/common/popup/popup.js
+++ pages/common/popup/popup.js
@@ -29,8 +29,87 @@
   function onConfirm() {
     this.hideModal();
   }
-module.exports.showDialogBtn = showDialogBtn
-module.exports.preventTouchMove = preventTouchMove
-module.exports.hideModal = hideModal
-module.exports.onCancel = onCancel
-module.exports.onConfirm = onConfirm
+function popupanim(that) {
+  console.log("createanim");
+  var anim = wx.createAnimation({
+    duration: 20,
+    timingFunction: 'linear',
+    transformOrigin: '50% 50% 0',
+  })
+  anim.scale(1, 1).step(0);
+  that.setData({
+    popupanim:anim.export()
+  })
+  setTimeout(()=>{
+    anim = wx.createAnimation({
+      duration: 40,
+      timingFunction: 'linear',
+      transformOrigin: '50% 50% 0',
+    })
+    anim.scale(1.1, 1.1).step()
+    anim.scale(1, 1).step()
+    that.setData({
+      popupanim: anim.export(),
+    })
+  },22)
+}
+
+
+function opacanim(that){
+  console.log("opacanim")
+  var anim = wx.createAnimation({
+    duration: 10,
+    timingFunction: 'linear',
+    transformOrigin: '50% 50% 0',
+  })
+  anim.opacity(0).step();
+  that.setData({
+    opacanim: anim.export()
+  })
+  setTimeout(() => {
+    anim = wx.createAnimation({
+      duration: 100,
+      timingFunction: 'linear',
+      transformOrigin: '50% 50% 0',
+    })
+    anim.opacity(0.5).step()
+    that.setData({
+      opacanim: anim.export(),
+    })
+  }, 12)
+}
+
+function opacanim_reverse(that){
+  console.log("opacanim_reverse")
+  var anim = wx.createAnimation({
+    duration: 10,
+    timingFunction: 'linear',
+    transformOrigin: '50% 50% 0',
+  })
+  anim.opacity(0.5).step();
+  that.setData({
+    opacanim: anim.export()
+  })
+  setTimeout(() => {
+    anim = wx.createAnimation({
+      duration: 50,
+      timingFunction: 'linear',
+      transformOrigin: '50% 50% 0',
+    })
+    anim.opacity(0).step()
+    that.setData({
+      opacanim: anim.export(),
+    })
+  }, 12)
+}
+
+module.exports = {
+  popupanim: popupanim,
+  showDialogBtn: showDialogBtn,
+  preventTouchMove: preventTouchMove,
+  hideModal: hideModal,
+  onCancel: onCancel,
+  onConfirm: onConfirm,
+  opacanim: opacanim,
+  opacanim_reverse: opacanim_reverse
+}
