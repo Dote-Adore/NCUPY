ef9155f84e00b9f08a023a2ce4955e819f5919b6
Dote-Adore <1379382440@qq.com>
2019-08-09 20:23:37 +0800

    1.0.14

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -6,142 +6,146 @@ Page({
     url: app.globalData.url,
     maincolor: app.globalData.maincolor,
 
-    publishid:0,
-    images:[],
-    price:'',
-    introduction:'',
+    publishid: 0,
+    images: [],
+    price: '',
+    introduction: '',
     tags: {
-      choosentags:[],
+      choosentags: [],
       classify: ''
     },
     tagarray: '',
 
     deviceWidth: app.globalData.windowWidth,
-    items: [
-      { name: 'new', value: '全新' },
-      { name: 'noBargain', value: '不讲价' }
+    items: [{
+        name: 'new',
+        value: '全新'
+      },
+      {
+        name: 'noBargain',
+        value: '不讲价'
+      }
     ],
-    
 
 
   },
-  onLoad(){
+  onLoad() {
     var that = this;
     console.log(that.data.tags);
   },
-  onShow(){
+  onShow() {
     var that = this;
-    var finaltagarray=[];
-    for(var i = 0;i<that.data.tags.choosentags.length;i++){
-      for(var j=0;j<that.data.items.length;j++)
-        if (that.data.tags.choosentags[i]===that.data.items[j].name){
+    var finaltagarray = [];
+    for (var i = 0; i < that.data.tags.choosentags.length; i++) {
+      for (var j = 0; j < that.data.items.length; j++)
+        if (that.data.tags.choosentags[i] === that.data.items[j].name) {
           finaltagarray.push(that.data.items[j].value);
         }
     }
     finaltagarray.push(that.data.tags.classify.value);
-    console.log("tagarry:"+finaltagarray)
+    console.log("tagarry:" + finaltagarray)
     that.setData({
       tagarray: finaltagarray
     })
-    if(that.data.tagarray===null)
-    that.setData({
-      tagarray:'>'
-    })
+    if (that.data.tagarray === null)
+      that.setData({
+        tagarray: '>'
+      })
   },
-  getIntroduction(e){
+  getIntroduction(e) {
     this.data.introduction = e.detail.value;
     console.log(this.data.introduction);
   },
-  getPrice(e){
+  getPrice(e) {
     this.data.price = e.detail.value;
   },
 
-  chooseImage(e){
+  chooseImage(e) {
     wx.chooseImage({
       success: this.handleChooseImageSucc.bind(this)
     })
   },
 
 
-  handleChooseImageSucc(res){//选择图片
+  handleChooseImageSucc(res) { //选择图片
     var that = this;
     const images = res.tempFilePaths;
     console.log(images);
     var choosenimagesArr = that.data.images;
-    if (choosenimagesArr.length+images.length<=9){
-      for (var i = 0; i < images.length;i++)
-      choosenimagesArr.push(images[i]);
+    if (choosenimagesArr.length + images.length <= 9) {
+      for (var i = 0; i < images.length; i++)
+        choosenimagesArr.push(images[i]);
       console.log(choosenimagesArr);
       that.setData({
         images: choosenimagesArr
       })
     }
-   
+
   },
-  removeImage(e){
+  removeImage(e) {
     console.log("removepics:" + e.target.dataset.idx);
     var that = this;
     const idx = e.target.dataset.idx;
     const resultimage = this.data.images;
     resultimage.splice(idx, 1);
     that.setData({
-      images:resultimage
+      images: resultimage
     })
   },
-  handleImagePreview(e){
+  handleImagePreview(e) {
     console.log(e);
     const idx = e.target.dataset.idx;
     const images = this.data.images;
     wx.previewImage({
-      current: images[idx],  //当前预览的图片
-      urls: images,  //所有要预览的图片
+      current: images[idx], //当前预览的图片
+      urls: images, //所有要预览的图片
     });
   },
-  chooseTag(){
+  chooseTag() {
     console.log("openTag");
     wx.navigateTo({
       url: 'tags/tags'
     })
   },
-  startPublish(){ //发布信息
-  if(app.globalData.userInfo===''){
-    login.authorization();
-    return;
-  }
+  startPublish() { //发布信息
+    if (app.globalData.userInfo === '') {
+      login.authorization();
+      return;
+    }
     console.log(this.data.tagarray)
-    if(app.globalData.userid===0)
-    {
+    if (app.globalData.userid === 0) {
       login.login()
-    } else if (this.data.price === '' || this.data.price === 0){
+    } else if (this.data.price === '' || this.data.price === 0) {
       wx.showToast({
         title: '请输入你想卖出的价格哦~',
         icon: "none"
       });
-    }else if(this.data.introduction===''){
+    } else if (this.data.introduction === '') {
       wx.showToast({
         title: '请介绍一下您的物品吧！',
         icon: "none"
       });
 
-    }else if(!this.data.tagarray[0]){
+    } else if (!this.data.tagarray[0]) {
       wx.showToast({
         title: '请选择一下标签，方便更好的卖出去哦~',
         icon: "none"
       });
-    }else if(!this.data.images[0]) {
+    } else if (!this.data.images[0]) {
       wx.showToast({
         title: '请选择至少一张图片哦~',
         icon: "none"
       });
-    }else {
-    wx.showLoading({
-      title: '正在发布',
-    });
-    this.uploadTextinfo();
+    } else {
+      wx.showLoading({
+        title: '正在发布',
+        mask: true
+      });
+      this.uploadTextinfo();
     }
   },
 
-  uploadTextinfo(){//上传文本信息
+  uploadTextinfo() { //上传文本信息
     var that = this;
     var commontags = [];
     for (var i = 0; i < that.data.tags.choosentags.length; i++) {
@@ -152,41 +156,41 @@ Page({
     }
     wx.request({
       url: this.data.url + "/publish/publishgoods",
-      data:{
-        price:this.data.price,
-        introduction:this.data.introduction,
-        category:this.data.tags.classify.index,
-        commontags:commontags,
-        userid:getApp().globalData.userid
+      data: {
+        price: this.data.price,
+        introduction: this.data.introduction,
+        category: this.data.tags.classify.index,
+        commontags: commontags,
+        userid: getApp().globalData.userid
       },
       //成功上传文本信息后上传图片
-      success:this.uploadimages.bind(this),
-      fail(res){
+      success: this.uploadimages.bind(this),
+      fail(res) {
         console.log(res);
       }
     })
   },
 
-  uploadimages(res) {//压缩加上传图片图片s
-  var that = this;
+  uploadimages(res) { //压缩加上传图片图片s
+    var that = this;
     console.log(res.data);
-    that.data.publishid = res.data;//获得发布号码
-    for(let i = 0;i<that.data.images.length;i++){
+    that.data.publishid = res.data; //获得发布号码
+    for (let i = 0; i < that.data.images.length; i++) {
       //压缩图片
-     wx.compressImage({
-      src: that.data.images[i],
-      quality: 20,
-       success(res) {
-         console.log("压缩图片：" + i + '成功');
+      wx.compressImage({
+        src: that.data.images[i],
+        quality: 20,
+        success(res) {
+          console.log("压缩图片：" + i + '成功');
           //成功压缩一张图片后上传一张图片
           that.data.images[i] = res.tempFilePath;
           that.uploadAImg(i);
-       },
-       fail(res){
-         that.uploadAImg(i);
+        },
+        fail(res) {
+          that.uploadAImg(i);
 
-       }
-     })      
+        }
+      })
     }
   },
 
@@ -231,7 +235,7 @@ Page({
       },
       fail(res) {
         console.log(res);
-        
+
       }
     })
   }
